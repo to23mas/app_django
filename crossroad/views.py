af77b48296cc66b0e6742f5bed77ec44a70a9e96
mict11 <to23mas@gmail.com>
2022-02-22 15:34:03 +0100

    lots of new stuff

--- crossroad/views.py
+++ crossroad/views.py
@@ -8,12 +8,17 @@ def welcome_view(request):
     """app welcome page"""
     return render(request, 'crossroad/welcome.html')
 
-
 @login_required(login_url='/accounts/login/')
 def courses_overview_view(request):
     list_courses = Lesson.objects.all()
-
-    return render(request, 'crossroad/courses_overview.html', {'lessons': list_courses, })
+    listOne = list_courses[1]
+    # row = list_courses.count() // 5 + 1
+    row = 6 // 5 + 1
+    rows = [i for i in range(row)]
+    cols = [i for i in range(4)]
+    return render(request, 'crossroad/courses_overview.html', {'lessons': list_courses,
+                                                               'rows': rows,
+                                                               'cols': cols, })
 
 
 @login_required(login_url='/accounts/login/')
